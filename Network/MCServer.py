aa4cbdc21b961894fac80ede670299fb181621bc
InishowenTechnology Ltd <58892653+IOTECH-Donegal@users.noreply.github.com>
2022-02-13 13:29:56 +0000

    Testing

---
+++ Network/MCServer.py
@@ -0,0 +1,44 @@
+'''
+Multicast Server by: JOR
+Reads multicast packets from a particular address and port.
+
+Alpha: 13FEB22
+'''
+import socket
+import struct
+import settings.mc as settings
+
+# Set multicast information
+MCAST_GRP = settings.MCSERVER["MCAST_GROUP"]
+SERVER_ADDRESS = ('', settings.MCSERVER["PORT"])
+MCAST_IF_IP = settings.MCSERVER["IP_ADDRESS"]
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    s.bind(SERVER_ADDRESS)
+    group = socket.inet_aton(MCAST_GRP)
+    mreq = struct.pack('4s4s', group, socket.inet_aton(MCAST_IF_IP))
+    s.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+    while True:
+        print('Waiting to receive message')
+        data, address = s.recvfrom(1024)
+        print(f'received {len(data)} bytes from {address}')
+        print(data)
+
+
+
+
+
+
+
+#s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#s.bind(server_address)
+#group = socket.inet_aton(multicast_group)
+#mreq = struct.pack('4s4s', group, socket.inet_aton(MCAST_IF_IP))
+#s.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+#while True:
+#    print('Waiting to receive message')
+#    data, address = s.recvfrom(1024)
+#    print(f'received {len(data)} bytes from {address}')
+#    print(data)
