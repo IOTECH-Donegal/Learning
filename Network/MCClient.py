27202c453c724138180d8f94c21077a7505e8b59
John ORaw <john.oraw@iotech.ie>
2022-02-13 13:36:31 +0000

    First

---
+++ Network/MCClient.py
@@ -0,0 +1,26 @@
+'''
+Multicast client by: JOR
+Sends multicast packets to a particular address and port.
+
+Alpha: 13FEB22
+'''
+
+import socket
+import time
+from datetime import datetime
+import settings.mc as settings
+
+# Set multicast information
+MCAST_GRP = settings.MCCLIENT["MCAST_GROUP"]
+MCAST_PORT = settings.MCCLIENT["PORT"]
+MCAST_IF_IP = settings.MCCLIENT["IP_ADDRESS"]
+
+
+while True:
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) as s:
+        s.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_IF, socket.inet_aton(MCAST_IF_IP))
+        #s.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
+        message_text = f"IOTECH {datetime.now()}"
+        message = message_text.encode('utf-8')
+        s.sendto(message, (MCAST_GRP, MCAST_PORT))
+        time.sleep(1)
