06c1304e5c914e2c422d00cbc4360b198ac58a2d
InishowenTechnology Ltd <58892653+IOTECH-Donegal@users.noreply.github.com>
2022-02-13 14:05:04 +0000

    Testing

--- Network/UDPClient.py
+++ Network/UDPClient.py
@@ -9,7 +9,7 @@ Alpha: 13FEB22
 import socket
 import settings.udp as settings
 
-UDP_IP = settings.UDP["CLIENT_UDP_IPv4"]
+UDP_IP = settings.UDP["SERVER_UDP_IPv4"]
 UDP_PORT = settings.UDP["SERVER_PORT"]
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
--- Network/settings/udp.py
+++ Network/settings/udp.py
@@ -1,5 +1,5 @@
 UDP = {
-    "SERVER_UDP_IPv4": '192.168.5.217',
-    "CLIENT_UDP_IPv4": '192.168.5.230',
+    "SERVER_UDP_IPv4": '192.168.5.230',
+    "CLIENT_UDP_IPv4": '192.168.5.217',
     "SERVER_PORT": 23
 }
